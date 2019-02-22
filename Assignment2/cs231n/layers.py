d4d21facdb2c774c0c1fc84e323108e9307238b1
minlee077 <minlee077@naver.com>
2019-02-22 22:51:02 +0900

    Update layers.py

--- Assignment2/cs231n/layers.py
+++ Assignment2/cs231n/layers.py
@@ -451,7 +451,8 @@ def dropout_forward(x, dropout_param):
         # TODO: Implement training phase forward pass for inverted dropout.   #
         # Store the dropout mask in the mask variable.                        #
         #######################################################################
-        pass
+        U = (np.random.rand(*x.shape)<p) / p # scaling during training time
+        out = x*U # drop
         #######################################################################
         #                           END OF YOUR CODE                          #
         #######################################################################
@@ -459,7 +460,7 @@ def dropout_forward(x, dropout_param):
         #######################################################################
         # TODO: Implement the test phase forward pass for inverted dropout.   #
         #######################################################################
-        pass
+        out = x
         #######################################################################
         #                            END OF YOUR CODE                         #
         #######################################################################
